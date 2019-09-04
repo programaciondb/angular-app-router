1e5a677343dfcdd08f8e1802898ff7ceaf960c60
David Bousquet <david@programaciondb.cl>
2019-09-04 11:26:16 -0400

    Se agregan rutas y html basico de menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from 'src/app/componentes/listar/listar.component';
+import { CrearComponent } from 'src/app/componentes/crear/crear.component';
+import { EditarComponent } from 'src/app/componentes/editar/editar.component';
+import { BorrarComponent } from 'src/app/componentes/borrar/borrar.component';
+import { MenuComponent } from 'src/app/componentes/menu/menu.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "menu", pathMatch: "full"},
+  {path: "listar", component: ListarComponent},
+  {path: "crear", component: CrearComponent},
+  {path: "editar", component: EditarComponent},
+  {path: "borrar", component: BorrarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
