ffe729be2c89ab6cb287d9049554b4ce90ff86a5
David Bousquet <david@programaciondb.cl>
2019-09-04 23:14:08 -0400

    se agrega funcion editar

--- src/app/componentes/editar/editar.component.ts
+++ src/app/componentes/editar/editar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { LibrosInterface } from 'src/app/modelos/libros';
+import { LibrosService } from 'src/app/servicios/libros.service';
 
 @Component({
   selector: 'app-editar',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarComponent implements OnInit {
 
-  constructor() { }
+  public libro: LibrosInterface;
+  constructor(
+    private librosServicios: LibrosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params["id"];
+    this.getLibro(id);
+  }
+
+  public getLibro(id: number) {
+    var libroObservable = this.librosServicios.getLibro(id);
+    libroObservable.subscribe(
+      libroObtenido => this.libro = libroObtenido
+    );
+  }
+
+  public putLibro(id: number) {
+    var libroObservable = this.librosServicios.putLibro(id, this.libro);
+    libroObservable.subscribe(
+      libroObtenido => {
+        this.libro = libroObtenido;
+        this.router.navigate(["/listar"])
+      });
   }
 
 }
